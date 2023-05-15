c7fb708af2456cf36706b437828b764f62537572
Salem Yaslem <s@sy.sa>
2023-05-15 16:30:59 +0300

    last try to fix VCode issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-
-;(global as any).CSS = { supports: () => false } 
-
 export default defineNuxtConfig({
     css: [
-        'vuetify/lib/styles/main.sass',
+        'vuetify/lib/styles/main.css',
         '@mdi/font/css/materialdesignicons.min.css'
     ],
     ssr: false,
     build: {
         transpile: ['vuetify'],
-    },
-    // disable vite externals and ssr
-    vite: {
-        optimizeDeps: {
-            exclude: ['vuetify'],
-        },
-        ssr: {
-            noExternal: ['vuetify'],
-        }
     },
 })
 
