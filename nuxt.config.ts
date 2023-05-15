4f4b084b2acd654edf9ce4c99aab5f227f674600
Salem Yaslem <s@sy.sa>
2023-05-15 15:54:11 +0300

    try to fix css issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css',],
+    css: ['vuetify/lib/styles/main.sass', 'vuetify/lib/components/VCode/VCode.css', '@mdi/font/css/materialdesignicons.min.css',],
     ssr: false,
     build: {
         transpile: ['vuetify'],
