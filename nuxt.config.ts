01ec9fa3ec30c144399b2ea89b3f420134eebc1c
Salem Yaslem <s@sy.sa>
2023-05-29 17:44:05 +0300

    add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Extractify - Extract compressed files online and securely',
+            titleTemplate: '%s - Extractify',
+        }
+    }
     css: [
         'vuetify/lib/styles/main.css',
         '@mdi/font/css/materialdesignicons.min.css'
